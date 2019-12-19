876c6ac2e5ba07dcfb542d7e99375e578aff3bdf
Tom Dane <tdane@tesla.com>
2019-12-19 08:08:30 -0800

    Test linear deployment

--- hello/main.go
+++ hello/main.go
@@ -20,7 +20,7 @@ func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "Hello world again",
+		"message": "Hello Amy",
 	})
 
 	if err != nil {
